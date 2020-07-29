f2ba6a7f99c5a6660cfa08a3dea00878dfcf224c
cawauchi6204 <cawauchi6204@gmail.com>
2020-07-29 15:53:40 +0900

    before firebase install

--- src/pages/resultPage.tsx
+++ src/pages/resultPage.tsx
@@ -1,8 +1,10 @@
 import React, {FC} from 'react'
+import TopHeader from '../components/topPage/topHeader'
 
 const ResultPage: FC = () => {
     return (
         <div>
+            <TopHeader />
             検索結果だよ
         </div>
     )
